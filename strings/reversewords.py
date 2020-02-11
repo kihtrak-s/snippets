7bd751ef46032e18af1af8906ef8ea866092e304
Karthik Srinivasan <kihtrak-s@users.noreply.github.com>
2020-02-11 07:48:44 +0530

    Code to reverse the sentence without reversing individual words

---
+++ strings/reversewords.py
@@ -0,0 +1,17 @@
+#input:
+#First line: an integer that represents the number of testcases
+#Followed by as many test cases. Each test case is a sentence where words are separated by dot('.')
+
+#Output:
+#The sentences where the last word becomes the first and vice versa
+
+def reversewords():
+    no_of_testcases = int(input())
+    for case in range(no_of_testcases):
+        sentence = input()
+        split_array = sentence.split('.')
+        new_sentence = '.'.join(reversed(split_array))
+        print(new_sentence)
+
+if __name__ == '__main__':
+    reversewords()
